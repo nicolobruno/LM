a32c19572df1126805547aa78c05d63eacbd40d4
nicolas <nicolas@cookunity.com>
2022-01-14 22:46:24 -0300

    Add shimmer image

--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import dayjs from 'dayjs';
 
 import arrow from '../../assets/arrow-white.png';
+import { useLoadImage } from '../../hooks/useLoadImage'
 import './styles.scss';
 
 const Quote = ({date, id, image}) => {
+    const loaded = useLoadImage(image)
     return (
         <Link to={`/quote/${id}`}>
-            <div className="picture-card" style={{ backgroundImage: `url(${image})` }}> 
+            <div className={`picture-card ${!loaded ? 'loading' : ''}`} style={ loaded ? { backgroundImage: `url(${loaded})` } : {}}>
                 <div className="date">
                     <span className="text">{dayjs(date).format('dddd, MMMM D, YYYY')}</span>
                     <img src={arrow} alt="arrow" className="arrow-date"/>
---
+++ src/hooks/useLoadImage.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+export const useLoadImage = src => {  
+    const [sourceLoaded, setSourceLoaded] = useState(null)
+  
+    useEffect(() => {
+      const img = new Image()
+      img.src = src
+      img.onload = () => setSourceLoaded(src)
+    }, [src])
+  
+    return sourceLoaded 
+  }
